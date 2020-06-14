181fb96924989a693d88bbec708e511a3576d2e0
meaghankoch <msk466@stern.nyu.edu>
2020-06-14 16:45:46 -0400

    Update robo_advisor.py
    Looping through to write all daily prices to CSV

--- App/robo_advisor.py
+++ App/robo_advisor.py
@@ -89,16 +89,19 @@ csv_file_path = os.path.join(os.path.dirname(__file__), "..", "data", "prices.cs
 
 csv_headers = ["timestamp", "open", "high", "low", "close", "volume"]
 with open(csv_file_path, "w") as csv_file: # "w" means "open the file for writing"
-    writer = csv.DictWriter(csv_file, fieldnames=["city", "name"])
+    writer = csv.DictWriter(csv_file, fieldnames=csv_headers)
     writer.writeheader() # uses fieldnames set above
 
-    writer.writerow({
-        "timestamp": "To Do",
-        "open":"To Do",
-        "high": "To Do",
-        "low": "To Do", 
-        "close": "To Do",
-        "volume": "To Do"
+
+    for date in dates: 
+        daily_prices = tsd[date]
+        writer.writerow({
+            "timestamp": date,
+            "open": daily_prices["1. open"],
+            "high": daily_prices["2. high"],
+            "low": daily_prices["3. low"], 
+            "close": daily_prices["4. close"],
+            "volume": daily_prices["5. volume"]
     })
 
 
