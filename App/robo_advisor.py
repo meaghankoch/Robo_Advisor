311c6ce6c80c8fea8320ea441f141dfa3b9753c2
meaghankoch <msk466@stern.nyu.edu>
2020-06-14 15:37:01 -0400

    Update robo_advisor.py
    Completed Latest Close & Last Refreshed with formatting

--- App/robo_advisor.py
+++ App/robo_advisor.py
@@ -11,33 +11,49 @@ import requests
 import json
 
 
+
 request_url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&apikey=demo"
 response = requests.get(request_url)
 #print(type(response)) --> requests.models.Response'
 #print(response.status_code) #--> 200
 #print(response.text)
 
+
+
+def to_usd(my_price):
+    """
+    Converts a numeric value to usd-formatted string, for printing and display purposes.
+
+    Param: my_price (int or float) like 4000.444444
+
+    Example: to_usd(4000.444444)
+
+    Returns: $4,000.44
+    """
+    return f"${my_price:,.2f}" #> $12,000.71
+
+
+
 #changing data to dict from str
 parsed_response = json.loads(response.text)
+
+#Data Investigation
 #print(type(parsed_response))
-print(parsed_response)
+#print(parsed_response.keys())
+#print(parsed_response["Meta Data"])
+#print(parsed_response["Time Series (Daily)"])
+# print(parsed_response)
+#print(parsed_response["Meta Data"].keys())
+last_refreshed = parsed_response["Meta Data"]["3. Last Refreshed"]
+print(last_refreshed)
+
+latest_close = parsed_response["Time Series (Daily)"]["2020-06-11"]["4. close"]
+print(latest_close)
 
 
 
-
-quit()
 
 #Info inputs
-
-
-
-
-
-
-#
-#
-#
-#
 
 
 
@@ -48,10 +64,10 @@ print("-------------------------")
 print("SELECTED SYMBOL: XYZ")
 print("-------------------------")
 print("REQUESTING STOCK MARKET DATA...")
-print("REQUEST AT: 2018-02-20 02:00pm")
+print("REQUEST AT:" )
 print("-------------------------")
-print("LATEST DAY: 2018-02-20")
-print("LATEST CLOSE: $100,000.00")
+print(f"LATEST DAY:{last_refreshed}")
+print(f"LATEST CLOSE:{to_usd(float(latest_close))}")
 print("RECENT HIGH: $101,000.00")
 print("RECENT LOW: $99,000.00")
 print("-------------------------")
@@ -60,3 +76,6 @@ print("RECOMMENDATION REASON: TODO")
 print("-------------------------")
 print("HAPPY INVESTING!")
 print("-------------------------")
+
+
+
